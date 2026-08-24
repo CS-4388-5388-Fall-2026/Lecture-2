5b410011c3f7625292ee9944ffedd74ab3e60206
Isayas Adhanom <isayasadhanom@gmail.com>
2026-08-24 12:28:44 -0500

    post-class update

--- src/App.ts
+++ src/App.ts
@@ -8,17 +8,23 @@ import * as gfx from 'gophergfx'
 
 export class App extends gfx.GfxApp
 {
+    private testBox: gfx.Mesh2;
     // --- Create the App class ---
     constructor()
     {
         // initialize the base class gfx.GfxApp
         super();
+        this.testBox = gfx.Geometry2Factory.createBox();
     }
 
 
     // --- Initialize the graphics scene ---
     createScene(): void 
     {
+        this.scene.add(this.testBox);
+        this.testBox.material.color = new gfx.Color(0.5, 0, 0.5, 1.0);
+        this.renderer.background = new gfx.Color(0,0,1,1);
+        this.testBox.position.set(0,0);
 
     }
 
@@ -26,6 +32,8 @@ export class App extends gfx.GfxApp
     // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
+        this.testBox.position.x += 0.001;
+        this.testBox.rotation += 0.01;
 
     }
 }
